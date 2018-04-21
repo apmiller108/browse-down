48ca08636f3115f03cfb6405eaf95a2d31f99cc1
Alex Miller <apmiller108@yahoo.com>
2018-04-21 09:18:57 -0400

    First commit

---
+++ lib/browse_down.ex
@@ -0,0 +1,18 @@
+defmodule BrowseDown do
+  @moduledoc """
+  Documentation for BrowseDown.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BrowseDown.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/browse_down_test.exs
@@ -0,0 +1,8 @@
+defmodule BrowseDownTest do
+  use ExUnit.Case
+  doctest BrowseDown
+
+  test "greets the world" do
+    assert BrowseDown.hello() == :world
+  end
+end
